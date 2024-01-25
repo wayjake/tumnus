3c003c95e2bf1c948950add028c4045ddf71d74d
Jake <jake@xyzdigital.com>
2024-01-25 12:58:23 -0800

    WIP - just trying things out

--- database/database.ts
+++ database/database.ts
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb')
+import { MongoClient, ServerApiVersion } from 'mongodb'
 
 const uri = process.env.mongo_uri
 if (!uri) {
--- index.ts
+++ index.ts
@@ -2,17 +2,23 @@ import { db } from './database/database'
 
 async function main() {
   try {
-    const item = await db.collection('registry').findOne({ name: 'Bun' })
-    if (item) {
-      throw new Error('Bun is already registered within the registry.')
-    }
-    await db.collection('registry').insertOne({
-      name: 'Bun',
+    /**
+      const item = await db.collection('registry').findOne({ name: 'Bun' })
+      if (item) {
+        throw new Error('Bun is already registered within the registry.')
+      }
+    */
+    const update = {
+      name: 'bun',
       description: 'Lifecycle events from the bun runtime.',
-      avro: 'avro schema',
+      schema: null,
       currentVersion: 1,
       previousVersions: [],
-    })
+    }
+    const registery = await db
+      .collection('registry')
+      .findOneAndUpdate({ name: update.name }, { $set: update }, { upsert: true })
+    console.table(registery)
   } catch (err) {
     console.error(err)
   }
